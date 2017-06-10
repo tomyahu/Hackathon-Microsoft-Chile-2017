3a04f9fa3e51aa218abc139bdfe2c4a45a314fff
adderou <dudu@adderou.cl>
2017-06-10 19:20:43 -0400

    Adding add product

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(function () {
   $('#agregar-producto').on('click', function(e) {
     var name = $('#name').val();
     var price = $('#price').val();
-    if !(name == "" || price == "") {
+    if (!(name == "" || price == "")) {
       socket.emit('add-product', {name: name, price: price});
       $('#name').val("");
       $('#price').val("");
