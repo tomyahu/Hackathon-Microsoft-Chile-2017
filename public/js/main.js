44a83cb097de6b4bdee78cfa12e15f1c851e52dd
Atemo678 <tomyahu@gmail.com>
2017-06-10 20:27:31 -0400

    Fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 var state;
 var email;
 $(function () {
+	
+  pedirNombre(); 
+
   var socket = io();
   socket.on('state', function(s) {
     console.log("state!");
@@ -17,17 +20,17 @@ $(function () {
           }
         }
       });
-      html += '<tr>
-                    <td>
-                    <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-data-table__select mdl-js-ripple-effect--ignore-events is-upgraded" data-upgraded=",MaterialCheckbox,MaterialRipple"><input type="checkbox" class="mdl-checkbox__input" '+  (me ? ' checked ' : '') +'><span class="mdl-checkbox__focus-helper"></span><span class="mdl-checkbox__box-outline"><span class="mdl-checkbox__tick-outline"></span></span><span class="mdl-checkbox__ripple-container mdl-js-ripple-effect mdl-ripple--center" data-upgraded=",MaterialRipple"><span class="mdl-ripple"></span></span></label>
-                    </td>
-                    <td class="mdl-data-table__cell--non-numeric big-text">'+p.name+'</td>
-                    <td>'+p.price+'</td>
-                    <td>'+len(inproduct) +'</td>
-                    <td>'+p.price / len(inproduct)+'</td>
+      html += '<tr>\
+                    <td>\
+                    <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-data-table__select mdl-js-ripple-effect--ignore-events is-upgraded" data-upgraded=",MaterialCheckbox,MaterialRipple"><input type="checkbox" class="mdl-checkbox__input" '+  (me ? ' checked ' : '') +'><span class="mdl-checkbox__focus-helper"></span><span class="mdl-checkbox__box-outline"><span class="mdl-checkbox__tick-outline"></span></span><span class="mdl-checkbox__ripple-container mdl-js-ripple-effect mdl-ripple--center" data-upgraded=",MaterialRipple"><span class="mdl-ripple"></span></span></label>\
+                    </td>\
+                    <td class="mdl-data-table__cell--non-numeric big-text">'+p.name+'</td>\
+                    <td>'+p.price+'</td>\
+                    <td>'+len(inproduct) +'</td>\
+                    <td>'+p.price / len(inproduct)+'</td>\
                      <td class="big-text">'+ inproduct.reduce(function(p,v) {
                        return v + ", " + p.name;
-                     }) +'</td>
+                     }) +'</td>\
                   </tr>';
     });
     console.log(html);
@@ -47,3 +50,25 @@ $(function () {
   });
 
 });
+
+function pedirMail() {
+    var txt;
+    email = prompt("Ingresa tu Mail:", "soylacomadreja@yomail.com");
+    if (email == null || mail == "") {
+        txt = "Error: mail inválido";
+    } else {
+        
+    }
+}
+
+function pedirNombre() {
+    var txt;
+    name = prompt("Ingresa tu nombre:", "Jorge Perez");
+    if (name == null || name == "") {
+        txt = "Error: nombre inválido";
+    } else {
+		//cosas de eduardo
+		
+        pedirMail();
+    }
+}
